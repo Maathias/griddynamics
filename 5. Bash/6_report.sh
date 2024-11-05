df126e28fa153c77cab35d10439dea782aea31ed
Mateusz Pstrucha <mpstrucha@griddynamics.com>
2024-11-05 14:18:35 +0100

    pretty format for report

--- 5. Bash/6_report.sh
+++ 5. Bash/6_report.sh
@@ -15,7 +15,7 @@ report_file=${1:-/dev/stdout}
 
 current_date=$(date +"%Y-%m-%d %H:%M:%S")
 current_user=$(whoami)
-internal_ip=$(ifconfig | grep 'inet ' | sed -n '2p' | cut -d' ' -f2)
+internal_ip=$(ifconfig | grep 'inet ' | sed -n '2p' | tr -s ' ' | cut -d' ' -f3)
 hostname=$(hostname)
 external_ip=$(curl -s ifconfig.me)
 distribution=$(cat /etc/*-release | grep "PRETTY_NAME" | cut -d\" -f2)
@@ -25,18 +25,19 @@ ram=$(free -h | grep Mem | tr -s ' ' | cut -d' ' -f2,4)
 cpu_cores=$(lscpu | grep "^CPU(s):" | cut -d: -f2 | xargs)
 cpu_frequency=$(lscpu | grep "CPU max MHz" | cut -d: -f2 | xargs)
 
-echo "Report" > $report_file
-echo "Date: $current_date" >> $report_file
-echo "User: $current_user" >> $report_file
-echo "Internal IP: $internal_ip" >> $report_file
-echo "Hostname: $hostname" >> $report_file
-echo "External IP: $external_ip" >> $report_file
-echo "Linux distribution: $distribution" >> $report_file
-echo "Uptime: $uptime" >> $report_file
-echo "Disk space used/free: $disk_space" >> $report_file
-echo "RAM: $ram" >> $report_file
-echo "CPU cores: $cpu_cores" >> $report_file
-echo "CPU frequency: $cpu_frequency" >> $report_file
+cat <<EOF > $report_file
+Report @ $current_date
+User:           $current_user
+Hostname:       $hostname
+Internal IP:    $internal_ip
+External IP:    $external_ip
+Linux distro:   $distribution
+Uptime:         $uptime
+Disk used/free: $disk_space
+RAM:            $ram
+CPU cores:      $cpu_cores
+CPU clock:      $cpu_frequency
+EOF
 
 echo "Report has been generated"
 [ -n "$1" ] && echo "File: $report_file"
